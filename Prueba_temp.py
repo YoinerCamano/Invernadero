a095ee459e908ed13da7b54ba87e9ea4c8a5798f
Yoiner Camaño <yoinerelias1996@gmail.com>
2023-03-17 17:48:20 -0500

    Envio_MQTT a servidor local
    se hace envio de mensaje mqtt con los valores de humedad y temperatura, separado por coma (,)

--- Prueba_temp.py
+++ Prueba_temp.py
@@ -1,23 +1,31 @@
-import machine
-import time
+from machine import SoftI2C, Pin
+from umqtt.simple import MQTTClient
+import time, network
 
-# Definir las constantes para la dirección y registro del sensor
+# Configuración de la red WiFi
+SSID = "CONSOLA X32"
+PASSWORD_WLAN = "X32.consola"
+
+# Configuración de la conexión MQTT
+SERVER = "192.168.0.103"
+PORT = 1883
+USER = "guest"
+PASSWORD = "guest"
+CLIENT_ID = "Sensor_Esp32"
+TOPIC = b'Cola_Temperatura'
+
+# Definir las constantes para la dirección y registros del sensor
 DEVICE_ADDRESS = 0x76
 REG_TEMP_MSB = 0xFA
 REG_TEMP_LSB = 0xFB
 REG_TEMP_XLSB = 0xFC
 REG_HUM_MSB = 0xFD
 REG_HUM_LSB = 0xFE
-REG_HUM_XLSB = 0xFF
-REG_PRESS_MSB = 0xF7
-REG_PRESS_LSB = 0xF8
-REG_PRESS_XLSB = 0xF9
-
-MODE_PRES_HIGHRES = 0x34
-MODE_TEMP_STANDARD = 0x2E
 
 # Inicializar el bus I2C
-i2c = machine.SoftI2C(scl=machine.Pin(22), sda=machine.Pin(21))
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=1000000)
+
+# Modo de operacion del sensor
 
 i2c.writeto_mem(DEVICE_ADDRESS,0xF2,bytes([0x02]))
 i2c.writeto_mem(DEVICE_ADDRESS,0xF4,bytes([0x93]))
@@ -39,7 +47,18 @@ calib_x = [
     (calib_data[19] << 8) | calib_data[18],
     (calib_data[21] << 8) | calib_data[20],
     (calib_data[23] << 8) | calib_data[22]
-]
+    ]
+
+# Conectar a la red WiFi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+if not wlan.isconnected():
+    print("Conectando a la red WiFi...")
+    wlan.connect(SSID,PASSWORD_WLAN)
+    while not wlan.isconnected():
+        pass
+    print("Conexión WiFi establecida. Dirección IP:", wlan.ifconfig()[0])
+
 def calculos(calib):
     # Leer los registros de temperatura del sensor
     temp_msb = i2c.readfrom_mem(DEVICE_ADDRESS, REG_TEMP_MSB, 1)[0]
@@ -47,49 +66,42 @@ def calculos(calib):
     temp_xlsb = i2c.readfrom_mem(DEVICE_ADDRESS, REG_TEMP_XLSB, 1)[0]
 
     # Leer los registros de humedad del sensor
-
     hum_msb = i2c.readfrom_mem(DEVICE_ADDRESS, REG_HUM_MSB, 1)[0]
     hum_lsb = i2c.readfrom_mem(DEVICE_ADDRESS, REG_HUM_LSB, 1)[0]
 
-    # Leer los registros de presión del sensor
-    press_msb = i2c.readfrom_mem(DEVICE_ADDRESS, REG_PRESS_MSB, 1)[0]
-    press_lsb = i2c.readfrom_mem(DEVICE_ADDRESS, REG_PRESS_LSB, 1)[0]
-    press_xlsb = i2c.readfrom_mem(DEVICE_ADDRESS, REG_PRESS_XLSB, 1)[0]
-
     # Convertir los valores de los registros en grados Celsius
+
     adc_T = (temp_msb << 12) | (temp_lsb << 4) | (temp_xlsb >> 4)
     var1 = ((((adc_T >> 3) - (calib[0] << 1))) * (calib[1])) >> 11
     var2 = (((((adc_T >> 4) - (calib[0])) * ((adc_T >> 4) - (calib[0]))) >> 12) * (calib[2])) >> 14
     t_fine = var1 + var2
     temperature = (t_fine * 5 + 128) >> 8
 
-    #Convertir los valores de los registros en presión (Pa)
-
-    adc_P = (press_msb << 12) | (press_lsb << 4) | (press_xlsb >> 4)
-    var1 = ((temperature) >> 1) - 64000
-    var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * int(calib[5])
-    var2 = var2 + ((var1 * int(calib[4])) << 1)
-    var2 = (var2 >> 2) + (int(calib[3]) << 16)
-    var1 = (((calib[2] * (((var1 >> 2) * (var1 >> 2)) >> 13)) >> 3) + ((calib[1] * var1) >> 1)) >> 18
-    var1 = ((32768 + var1) * int(calib[0])) >> 15
-    pressure = 0
-    if var1 != 0:
-        pressure = ((1048576 - adc_P) - (var2 >> 12)) * 3125
-        if pressure < 0x80000000:
-            pressure = (pressure << 1) // var1
-        else:
-            pressure = (pressure // var1) * 2
-        var1 = (int(calib[8]) * (((pressure >> 3) * (pressure >> 3)) >> 13)) >> 12
-        var2 = ((pressure >> 2) * int(calib[7])) >> 13
-        pressure = pressure + ((var1 + var2 + int(calib[6])) >> 4)
-
     # Convertir los valores de los registros en humedad relativa (%)
+
     adc_H = (hum_msb << 8) | hum_lsb
     humidity = ((adc_H / 2**16) * 100)
 
-    return([(temperature/100),pressure,humidity])
+    return([(temperature/100),humidity])
 
-# Imprimir el valor de temperatura en grados Celsius
 while True:
-    print(calculos(calib_x))
-    time.sleep(1)
+    temp = ""
+    # Conectar al servidor MQTT y publicar el mensaje
+    client = MQTTClient(CLIENT_ID, SERVER, PORT, USER, PASSWORD)
+    print("Conectando al servidor MQTT...")
+    client.connect()
+    for i in range(10):
+        print("Espera para enviar dato tiempo",(i+1),"Seg")
+        time.sleep(1)
+    for i in range(6):
+        out_sensor = calculos(calib_x)
+        temp = str(out_sensor[0]) + "," + str(round(out_sensor[1],2))
+        MESSAGE = temp
+        try:
+            client.publish(TOPIC, MESSAGE.encode('utf-8'), retain=False, qos=0)
+            print("Mensaje MQTT publicado en el tema", TOPIC, "Con los datos:", MESSAGE)
+        except OSError:
+            print("Error al conectar al servidor MQTT")
+        time.sleep(5)
+
+    client.disconnect()
