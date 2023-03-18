09e18ec701453c7410f7fcb198f3189aa4c768f6
Yoiner Camaño <yoinerelias1996@gmail.com>
2023-03-17 21:35:20 -0500

    Add
    Sin importancia

---
+++ Codigo_telegram.py
@@ -0,0 +1,39 @@
+import urequests as requests
+import json, network, time
+
+# Configuración de la red WiFi
+
+SSID = "CONSOLA X32"
+PASSWORD_WLAN = "X32.consola"
+
+# Configuración BOT TELEGRAM
+
+bot_token = "5839981812:AAH7AUgJNLoeo1E2LsZqb2ay8uGe4ecwE_I"
+chat_id = "1214775886"
+message = "Hello from ESP32!"
+
+# Conectar a la red WiFi
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+if not wlan.isconnected():
+    print("Conectando a la red WiFi...")
+    wlan.connect(SSID,PASSWORD_WLAN)
+    while not wlan.isconnected():
+        pass
+    print("Conexión WiFi establecida. Dirección IP:", wlan.ifconfig()[0])
+
+url = "https://api.telegram.org/bot{}/sendMessage".format(bot_token)
+data = {
+    "chat_id": chat_id,
+    "text": message
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+while True:
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    print(response.text)
+    time.sleep(10)
+
--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-from machine import SoftI2C, Pin
-import time
+import time, network
+from umqtt.simple import MQTTClient
 
-BMP280_ADDR = 0x76
-# Inicializar el bus I2C
+# Configuración de la red WiFi
+SSID = "CONSOLA X32"
+PASSWORD_WLAN = "X32.consola"
 
-i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
+# Configuración de la conexión MQTT
+SERVER = "100.26.209.37"
+PORT = 1883
+USER = "guest"
+PASSWORD = "guest"
+CLIENT_ID = "Sensor"
+TOPIC = "mensajes"
 
-# Escribir el comando de configuración de resolución de temperatura
+# Conectar a la red WiFi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+if not wlan.isconnected():
+    print("Conectando a la red WiFi...")
+    wlan.connect(SSID,PASSWORD_WLAN)
+    while not wlan.isconnected():
+        pass
+    print("Conexión WiFi establecida. Dirección IP:", wlan.ifconfig()[0])
 
-i2c.writeto(BMP280_ADDR, bytes([0xF4, 0x4B]))  # 0x27 es una resolución de 12 bits
-
+band = 0
 while True:
-    i2c.writeto(BMP280_ADDR, bytes([0xFA , 0xFB, 0xFC]))
-    print(i2c.readfrom(BMP280_ADDR,9)[0])
-    time.sleep(2)
+    # Conectar al servidor MQTT y publicar el mensaje
+    print("Conectando al servidor MQTT...")
+    client = MQTTClient(CLIENT_ID, SERVER, PORT, USER, PASSWORD)
+    client.connect()
+    MESSAGE = str(150)
+    band += 1
+    if band == 10:
+        band = 0
+    try:
+        client.publish(TOPIC, MESSAGE, retain=False, qos=0)
+        print("Mensaje MQTT publicado en el tema", TOPIC)
+    except OSError:
+        print("Error al conectar al servidor MQTT")
+    finally:
+        client.disconnect()
+    time.sleep(10)
